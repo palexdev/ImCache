ef6a5dbf39a6c410a117dfd038fcc3b0d8eaf633
palexdev <alessandro.parisi406@gmail.com>
2025-05-21 13:45:10 +0200

    :memo: Update copyright
    Signed-off-by: palexdev <alessandro.parisi406@gmail.com>

--- src/main/java/io/github/palexdev/imcache/utils/AsyncUtils.java
+++ src/main/java/io/github/palexdev/imcache/utils/AsyncUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2025 Parisi Alessandro - alessandro.parisi406@gmail.com
+ * This file is part of ImCache (https://github.com/palexdev/imcache)
+ *
+ * ImCache is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * ImCache is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ImCache. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.palexdev.imcache.utils;
 
 import java.util.concurrent.Callable;
